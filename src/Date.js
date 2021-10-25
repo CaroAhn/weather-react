7c32798c3177a6283ea4d65306058cfbbd270f57
CaroAhn <ahncaro93@gmail.com>
2021-10-25 12:32:03 -0400

    Update Date.js

--- src/Date.js
+++ src/Date.js
@@ -5,7 +5,7 @@ export default function Date() {
   return (
     <div className="date">
       <h1 className="current-day"> 18:20</h1>
-      <h1>Friday, June 25 </h1>
+      <h1>Monday, October 25 </h1>
     </div>
   );
 }
